b938fef54939b5bb280017310825a2d941898533
Your Name <you@example.com>
2020-03-20 07:14:27 +0900

    attempt
    Signed-off-by: Your Name <you@example.com>

--- src/socketiv.c
+++ src/socketiv.c
@@ -16,8 +16,8 @@
 
 #include "intr.c"
 
-typedef struct ivsm {
-	int sender_ack;
+typedef volatile struct ivsm {
+	int writer_ack;
 	int reader_ack;
 	bool poll_mode;
 	void *cts_queue;
@@ -33,7 +33,7 @@ typedef struct ivsm {
 #define TIMESTAMP_ENTRIES 20
 #define STORM_RATE_MS 50
 #define POLL_US 500
-typedef struct ivsock {
+typedef volatile struct ivsock {
 	int enabled;
 
 	// QoS 에 필요한 변수들
@@ -96,7 +96,7 @@ static inline int attach_new_ivsock_to_fd(int fd) {
 
 	ivsock->blk_size = 256 * 1024;
 	ivsock->ivsm_addr = plain_mmap; //(void*)PHYS_ADDR;
-	memset(plain_mmap, 0, sizeof(IVSOCK));
+	//memset(plain_mmap, 0, sizeof(IVSOCK));
 
 	return 0;
 }
