5b0c13a8d5bb350e0b654ffd417fa39209e87d3c
minmossi <alswg1994@gmail.com>
2020-06-14 19:29:03 +0900

    add debug

--- src/socketiv2.c
+++ src/socketiv2.c
@@ -86,9 +86,8 @@ ssize_t socketiv_read(int fd, void *buf, size_t count) {
 		if (ivsm->rptr + remain_cnt == ENDPOINT) {
 			puts("(rptr reached endpoint)");
 			ivsm->rptr = 0;
-		} else {
+		} else
 			ivsm->rptr += remain_cnt;
-		}
 		printf("ivsm->rtpr: %lu\n", ivsm->rptr);
 	}
 
@@ -107,7 +106,7 @@ static inline int64_t getmstime(void) {
 ssize_t socketiv_write(int fd, const void *buf, size_t count) {
 	IVSOCK *ivsock = fd_to_ivsock_map[fd];
 	size_t to_write = 0; // if 문 안에서 처리할 바이트
-	size_t remain_cnt = count, processed_byte = 0;
+	size_t remain_cnt = count, prev_remain_cnt = 0, processed_byte = 0;
 	IVSM *ivsm = ivsock->ivsm_addr_write;
 
 	while (remain_cnt) {
@@ -158,19 +157,20 @@ ssize_t socketiv_write(int fd, const void *buf, size_t count) {
 
 		// 마지막 쓰기
 		printf("WPTR: %lu, RPTR: %lu, fulled: %d, remain_cnt: %lu\n", ivsm->wptr, ivsm->rptr, ivsm->fulled, remain_cnt);
+		puts("(final write stage");
 		memcpy((void *)ivsm + OFFSET + ivsm->wptr, (void*)(buf + processed_byte), remain_cnt);
+		prev_remain_cnt = remain_cnt;
 		remain_cnt = 0;
-		if((ivsm->wptr + remain_cnt == ivsm->rptr) || (ivsm->wptr + remain_cnt == ENDPOINT))
+		if((ivsm->wptr + prev_remain_cnt == ivsm->rptr) || (ivsm->wptr + prev_remain_cnt == ENDPOINT))
 			ivsm->fulled = 1;
 
 		// 포인터가 엔드 포인트에 도달하면 0으로 변경
 		if (ivsm->wptr + remain_cnt == ENDPOINT) {
-			printf("1111\n");
+			puts("(wptr reached endpoint)");
 			ivsm->wptr = 0;
-		} else {
-			printf("2222\n");
+		} else
 			ivsm->wptr += remain_cnt;
-		}
+		printf("ivsm->wtpr: %lu\n", ivsm->wptr);
 	}
 
 	printf("IVSH: WRITE\n");
